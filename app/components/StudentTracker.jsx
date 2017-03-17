c8b8951211b71a120f439f358a9a5e85fe124c3a
Jonathan D. Mann <jdmann84@gmail.com>
2017-03-17 13:59:10 -0700

    converted parts of student template into respective components

--- app/components/StudentTracker.jsx
+++ app/components/StudentTracker.jsx
@@ -3,8 +3,72 @@ import React from 'react'
 
 const Tracker = ({user, assignments}) => {
   return (
-    <div>Tracker </div>
+		<div className="flex-container" id="student-tracker">
+
+      <div className="flex-child list-container">
+
+        <section className="panel list">
+          <div className="panel-header">Notifications</div>
+          <ul className="notifications-list">
+            <li className="overdue">Assignment X is overdue.<span className="icon icon-external-link-sqaure"></span></li>
+            <li>Assignment Y graded.<span className="icon icon-external-link-sqaure"></span></li>
+            <li>New Assigment Z added.<span className="icon icon-external-link-sqaure"></span></li>
+            <li>New Assigment W added.<span className="icon icon-external-link-sqaure"></span></li>
+            <li>New Assigment W added.<span className="icon icon-external-link-sqaure"></span></li>
+            <li>Assignment H graded.<span className="icon icon-external-link-sqaure"></span></li>
+          </ul>
+        </section> 
+
+      </div>
+      <div className="flex-child list-container">
+
+        <section className="panel list">
+          <div className="panel-header">Notifications</div>
+          <ul className="notifications-list">
+            <li className="overdue">Assignment X is overdue.<span className="icon icon-external-link-sqaure"></span></li>
+            <li>Assignment Y graded.<span className="icon icon-external-link-sqaure"></span></li>
+            <li>New Assigment Z added.<span className="icon icon-external-link-sqaure"></span></li>
+            <li>New Assigment W added.<span className="icon icon-external-link-sqaure"></span></li>
+            <li>New Assigment W added.<span className="icon icon-external-link-sqaure"></span></li>
+            <li>Assignment H graded.<span className="icon icon-external-link-sqaure"></span></li>
+          </ul>
+        </section> 
+
+      </div>
+      <div className="flex-child list-container">
+
+        <section className="panel list">
+          <div className="panel-header">Notifications</div>
+          <ul className="notifications-list">
+            <li className="overdue">Assignment X is overdue.<span className="icon icon-external-link-sqaure"></span></li>
+            <li>Assignment Y graded.<span className="icon icon-external-link-sqaure"></span></li>
+            <li>New Assigment Z added.<span className="icon icon-external-link-sqaure"></span></li>
+            <li>New Assigment W added.<span className="icon icon-external-link-sqaure"></span></li>
+            <li>New Assigment W added.<span className="icon icon-external-link-sqaure"></span></li>
+            <li>Assignment H graded.<span className="icon icon-external-link-sqaure"></span></li>
+          </ul>
+        </section> 
+
+      </div>
+      <div className="flex-child list-container">
+
+        <section className="panel">
+          <div className="panel-header">Notifications</div>
+          <ul className="notifications-list">
+            <li className="overdue">Assignment X is overdue.<span className="icon icon-external-link-sqaure"></span></li>
+            <li>Assignment Y graded.<span className="icon icon-external-link-sqaure"></span></li>
+            <li>New Assigment Z added.<span className="icon icon-external-link-sqaure"></span></li>
+            <li>New Assigment W added.<span className="icon icon-external-link-sqaure"></span></li>
+            <li>New Assigment W added.<span className="icon icon-external-link-sqaure"></span></li>
+            <li>Assignment H graded.<span className="icon icon-external-link-sqaure"></span></li>
+          </ul>
+        </section>    
+
+      </div>
+    </div>   
   )
 }
 
 export default Tracker
+
+
