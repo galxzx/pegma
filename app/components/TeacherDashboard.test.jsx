7705328263d042c5b336546f4e23e32514635611
kavispires <kavispires@gmail.com>
2017-03-21 14:02:17 -0700

    Created Teacher Dashboard and Assignments Front End tests

---
+++ app/components/TeacherDashboard.test.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import chai, {expect} from 'chai'                                                   
+chai.use(require('chai-enzyme')())
+import {shallow, mount} from 'enzyme'
+import {spy} from 'sinon'
+chai.use(require('sinon-chai'))
+import {createStore} from 'redux'
+
+import TeacherDashboard from './TeacherDashboard'
+import TeacherStats from './Dashboard/TeacherStats'
+import TeacherNotifications from './Dashboard/TeacherNotifications'
+import CalendarSmall from './Dashboard/CalendarSmall'
+import TeacherAssignmentsPanel from './Dashboard/TeacherAssignmentsPanel'
+
+
+describe('<TeacherDashboard/>', () => {
+
+  let root
+  beforeEach('render the root', () =>
+    root = shallow(<TeacherDashboard />)
+  )
+
+  it('should have a Student Stats panel', () => {
+    expect(root.find(TeacherStats)).to.have.length(1)
+  })
+
+  it('should have a Notifications panel', () => {
+    expect(root.find(TeacherNotifications)).to.have.length(1)
+  })
+
+  it('should have a Calendar panel', () => {
+    expect(root.find(CalendarSmall)).to.have.length(1)
+  })
+
+  it('should have a Assignments Panel', () => {
+    expect(root.find(TeacherAssignmentsPanel)).to.have.length(1)
+  })
+
+  xit('should receive assignments as props', () => {
+    expect(root.assignments).to.be.defined
+  })
+
+})
