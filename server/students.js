224af163cf217f160e09fd747013a777f6fc0a9e
Corryn Young <young.corryn@gmail.com>
2017-03-23 12:41:42 -0400

    load teacher/user/names with student

--- server/students.js
+++ server/students.js
@@ -33,7 +33,7 @@ module.exports = require('express').Router()
 		.then(assignment => res.json(assignment))
 		.catch(next))
 	.get('/:studentId/', mustBeLoggedIn, (req, res, next) =>
-		Student.findById(req.params.studentId, {include: [Teacher, Assignment]})
+		Student.findById(req.params.studentId, {include: [{model:Teacher, include:[{model:User, attributes:['firstName', 'lastName']}]}, {model: Assignment}]})
 		.then(student => res.json(student))
 		.catch(next))
 	.put('/:studentId/assignments/:assignmentId', (req, res, next) =>
