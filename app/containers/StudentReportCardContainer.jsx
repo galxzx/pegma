0c104b136c70575a63d8d67dfa35ddcd5298cd39
Corryn Young <young.corryn@gmail.com>
2017-03-23 12:47:27 -0400

    add teacher name to report card

--- app/containers/StudentReportCardContainer.jsx
+++ app/containers/StudentReportCardContainer.jsx
@@ -5,7 +5,8 @@ import StudentReportCard from '../components/StudentReportCard'
 const mapState = (state) => {
   return {
     user: state.auth,
-    assignments: state.student.assignments
+    assignments: state.student.assignments,
+    teacher: state.student.teacher
   }
 }
 export default connect(mapState)(StudentReportCard)
