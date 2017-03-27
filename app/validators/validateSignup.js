234601a9d10fc569cb054e90e1ad197c8fa5f3d6
kavispires <kavispires@gmail.com>
2017-03-27 11:48:40 -0700

    Refactored Sign Up

--- app/validators/validateSignup.js
+++ app/validators/validateSignup.js
@@ -1,7 +1,9 @@
 const validateSignup = values => {
   const errors = {}
+  if(values.avatar && !checkURL(values.avatar)) errors.avatar = 'Avatar must be an image URL'
   if(!values.name) errors.name = 'Name is required'
   if(!values.email) errors.email = 'Email is required'
+  if(values.email && !checkEmail(values.email)) errors.email = 'Email is invalid'
   if(!values.type) errors.type = 'Account Type is required'
   if(!values.password) errors.password = 'Password is required'
   if (values.password !== values.verPwd) {
@@ -9,10 +11,16 @@ const validateSignup = values => {
     errors.verPwd = 'Passwords must be the same'
   }
   if(values.type === 'student') {
-    if(!values.teacher) errors.type = "Teacher is required"
+    if(!values.teacher) errors.type = 'Teacher is required'
   }
   return errors
 }
 
-
 export default validateSignup
+
+const checkURL = (url) => (url.match(/\.(jpeg|jpg|gif|png)$/) !== null)
+
+const checkEmail = (email) => {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return email.match(re) !== null;
+}
