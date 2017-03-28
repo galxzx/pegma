85a06a9061696ff9d56c738aef2413956087b355
kavispires <kavispires@gmail.com>
2017-03-28 08:24:49 -0700

    Added calendar to teacher model

--- db/models/teacher.js
+++ db/models/teacher.js
@@ -3,6 +3,8 @@
 const Sequelize = require('sequelize')
 const db = require('APP/db')
 
-const Teacher = db.define('teachers', {})
+const Teacher = db.define('teachers', {
+	calendar: Sequelize.STRING
+})
 
 module.exports = Teacher
