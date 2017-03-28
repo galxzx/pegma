b3868f18523e96a957376e4eaed5ff2039b8169f
Corryn Young <young.corryn@gmail.com>
2017-03-28 17:20:56 -0400

    fix create quiz error rendering

--- app/components/RenderError.jsx
+++ app/components/RenderError.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
 const RenderError = ({ meta: { touched, error } }) => touched && error ?
-  <span className="formError">{error}</span> : false
+  <span className="icon icon-error-circle red">{error}</span> : false
 
 export default RenderError
