f2e22c09af607f1a0da39f907ccc96d9805c7a7d
Jonathan D. Mann <jdmann84@gmail.com>
2017-03-29 17:37:44 -0700

    added some possible styling features, lets review them before deciding to merge

--- app/components/TeacherFunctions.jsx
+++ app/components/TeacherFunctions.jsx
@@ -26,61 +26,74 @@ const TeacherFunctions = ({students, library, handleSubmit, toggleCheckAll, due_
       <div className="container panel-container functions">
         <div className="flex-container">
           <div className="flex-child panel">
-            <div className="panel-header">Control Panel</div>
+            <div className="panel-header">New Assignments</div>
             <section>
-              <p>Here you can set assignments to your students. You may use any assignment previously created or create your own tasks and quizzes.</p>
+              <p> Here you can set assignments to your students. You may use any assignment previously created or create your own tasks and quizzes.</p>
             </section>
             <section className="options">
               <Link to="/teacher/createtask"><button className="btn btn-primary">Create New Task</button></Link>
               <Link to="/teacher/createquiz"><button className="btn btn-primary">Create New Quiz</button></Link>
             </section>
+          </div>
+          <div className="flex-child panel">   
+            <div className="panel-header">Existing Assignments</div>
             <section className="teacher-assignments">
-              <h3>Assignments Panel</h3>
               <p>Select a quiz and/or task to assign to students.</p>
               <form onSubmit={(evt) => handleSubmit(evt)}>
-                <select id="tasks" name="tasks">
-                  <option>Assign Task...</option>
-                {
-                  library && library.tasks &&
-                    library.tasks.map(task =>
-                      <option
-                        id={`task-${task.id}`}
-                        key={task.id}
-                        data-id={task.id}
-                        data-title={task.title}>{task.title}
-                      </option>
-                    )
-                }
-                </select>
-
-                <select id="quizzes" name="quizzes">
-                  <option>Assign Quiz...</option>
-                {
-                  library && library.quizzes &&
-                    library.quizzes.map(quiz =>
-                      <option
-                        id={`quiz-${quiz.id}`}
-                        key={quiz.id}
-                        data-id={quiz.id}
-                        data-title={quiz.title}
-                        >{quiz.title}
-                      </option>
-                    )
-                }
-                </select>
-                <div className="due-date-picker">
-                  <label>Due Date </label>
-                  <DatePicker selected={ due_date } onChange={handleChange} />
+                <div className="flex-container">
+                  <div className="flex-child">                 
+                    <select id="tasks" name="tasks" className="normal">
+                      <option>Assign Task...</option>
+                    {
+                      library && library.tasks &&
+                        library.tasks.map(task =>
+                          <option
+                            id={`task-${task.id}`}
+                            key={task.id}
+                            data-id={task.id}
+                            data-title={task.title}>{task.title}
+                          </option>
+                        )
+                    }
+                    </select>
+                  </div>
+                  <div className="flex-child">                     
+                    <select id="quizzes" name="quizzes" className="normal">
+                      <option>Assign Quiz...</option>
+                    {
+                      library && library.quizzes &&
+                        library.quizzes.map(quiz =>
+                          <option
+                            id={`quiz-${quiz.id}`}
+                            key={quiz.id}
+                            data-id={quiz.id}
+                            data-title={quiz.title}
+                            >{quiz.title}
+                          </option>
+                        )
+                    }
+                    </select>
+                  </div>
                 </div>
-                <button type="submit" className="btn btn-primary">Assign Task</button>
-
+                <div className="flex-container">
+                    <div className="flex-child due-date-picker">                
+                      <label>Due Date </label>
+                      <DatePicker selected={ due_date } onChange={handleChange}  />
+                    </div>
+                    <div className="flex-child"> 
+                      <label></label>                                                       
+                      <button type="submit" className="btn btn-primary normal assign">Assign Task</button>
+                    </div>
+                  </div>
               </form>
-            </section>
-            <section>
-              <div id="message-box">{ message }</div>
-            </section>
+            </section>         
+          </div>
+        </div>
+        <div className="flex-container">
+          <div className="flex-child panel">          
+            <div className="panel-header">Select Students</div>            
             <section className="teacher-assignments">
-              <p>Select students to assign...</p>
+              <h3 id="message-box">{ message }</h3>
               <table className="teacher-assignments">
                 <tbody id="students">
                   <tr id="filters">
