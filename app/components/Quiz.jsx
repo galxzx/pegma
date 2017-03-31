a844fe5d6a2e86f28900ea2a72796ba9d64d56a2
Jonathan D. Mann <jdmann84@gmail.com>
2017-03-31 12:14:52 -0700

    centered quiz and completed quiz questions for screencast

--- app/components/Quiz.jsx
+++ app/components/Quiz.jsx
@@ -10,29 +10,35 @@ const required = value => (value || value === 0) ? undefined : 'Required'
 const Quiz = ({ handleSubmit, quiz, gradeQuiz, quizForm, user }) => {
   return (
     <form className="quiz" onSubmit={handleSubmit(gradeQuiz)}>
-      {quiz.questions.map((question, indexNum) => {
-        let questionNum = indexNum + 1;
-        return (
-          <div key={question.id} className="question-group">
-            <h4 className="inquiry">{questionNum}. {question.inquiry}</h4>
-            <ul className="inquiry-options">
-              {question.answer.map((answer, idx) =>{
-                return (
-                  <li key={answer} className="inquiry-option">
-                    <label>
-                      <Field name={'q_'+question.id} component="input" type="radio" value={idx} checked={quizForm && quizForm.values && quizForm.values['q_'+question.id] == idx} />
-                      {answer}
-                    </label>
-                  </li>
-                )
-              })}
-              <Field name={'q_'+question.id} component={renderError} validate={required} />
-            </ul>
-          </div>
-        )
-      })}
       <div>
-        <button className="btn btn-primary" type="submit" disabled={user.teacher_id}>Submit Quiz</button>
+        {quiz.questions.map((question, indexNum) => {
+          let questionNum = indexNum + 1;
+          return (
+            <div key={question.id} className="question-group center">
+              <div className="centered-block">            
+                <h4 className="inquiry">{questionNum}. {question.inquiry}</h4>
+                <ul className="inquiry-options">
+                  {question.answer.map((answer, idx) =>{
+                    return (
+                      <li key={answer} className="inquiry-option">
+                        <label>
+                          <Field name={'q_'+question.id} component="input" type="radio" value={idx} checked={quizForm && quizForm.values && quizForm.values['q_'+question.id] == idx} />
+                          {answer}
+                        </label>
+                      </li>
+                    )
+                  })}
+                  <Field name={'q_'+question.id} component={renderError} validate={required} />
+                </ul>
+              </div>
+            </div>
+          )
+        })}
+        <div className="center">
+          <div className="centered-block">
+            <button className="btn btn-primary" type="submit" disabled={user.teacher_id}>Submit Quiz</button>
+          </div>
+       </div>
       </div>
     </form>
 
